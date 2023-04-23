2840b5d006cf6fda1cfec2d9441f673dcd32363b
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2023-04-22 22:37:40 -0300

    added login and register structures with routes

--- src/components/AppRouter/index.jsx
+++ src/components/AppRouter/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from '../../pages/Main.jsx';
 import ChatPage from '../../pages/ChatPage.jsx';
+import Login from '../../pages/Login.jsx';
+import Register from '../../pages/Register.jsx';
+
 export default function AppRouter() {
   return (
     <Main>
       <Routes>
         <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Main>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,7 +5,7 @@ export default function Main(props) {
   return (
     <div className="vh-100 w-100 d-flex align-items-center justify-content-between flex-column">
       <header className="header">NAVBAR</header>
-      <main className="overflow-auto">{props.children}</main>
+      <main>{props.children}</main>
       <footer className="footer"></footer>
     </div>
   );
